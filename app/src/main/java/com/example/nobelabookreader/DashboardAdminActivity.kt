1e50414c0412f82e6e9ab9ac7576dd438aa012c9
ArthuroZareno <147407929+ArthuroZareno@users.noreply.github.com>
2024-03-05 11:23:46 +0800

    login

---
+++ app/src/main/java/com/example/nobelabookreader/DashboardAdminActivity.kt
@@ -0,0 +1,40 @@
+package com.example.nobelabookreader
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.nobelabookreader.databinding.ActivityDashboardAdminBinding
+
+class DashboardAdminActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityDashboardAdminBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDashboardAdminBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_dashboard_admin)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_dashboard_admin)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
--- app/src/main/java/com/example/nobelabookreader/MainActivity.kt
+++ app/src/main/java/com/example/nobelabookreader/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.nobelabookreader
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.nobelabookreader.databinding.ActivityMainBinding
@@ -16,11 +17,11 @@ class MainActivity : AppCompatActivity() {
 
         //login
         binding.loginBtn.setOnClickListener {
-            //will do later
+            startActivity(Intent(this, LoginActivity::class.java))
         }
         //continue without login
         binding.skipBtn.setOnClickListener {
-            //will do later
+            startActivity(Intent(this, DashboardUserActivity::class.java))
         }
     }
 }
